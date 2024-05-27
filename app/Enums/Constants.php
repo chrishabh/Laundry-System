bb077dcae9579a47347eb74e2eac205501159696
rchoudhary-at-770608018768 <ch.rishabh8527@gmail.com>
2024-05-27 23:39:35 +0530

    Revert "update for otp test case"
    This reverts commit a3d4467606f39fb6dc2836d59e232fa49db1a5ba.

--- app/Enums/Constants.php
+++ app/Enums/Constants.php
@@ -6,6 +6,5 @@ class Constants
 {
    // Batch constants
    const CLEAN_SERVER_DIR = "clean_server_directory";
-   const OTP_MESSAGE = "Dear Customer, Your OTP(One Time Password) for Phone Verification is ";
 
 }
