88ea2cfdf78f6c99f324576803f2d68f6aaae783
rchoudhary-at-770608018768 <ch.rishabh8527@gmail.com>
2024-06-03 16:09:25 +0530

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('phone_number')->unique();
             $table->timestamp('phone_verified_at')->nullable();
             $table->string('user_role')->nullable();
